80a1858fc8dad9cbc6116677a71390175dcf4f8c
jeffrepo <jeffer.leo93@gmail.com>
2025-01-03 07:18:01 -0600

    Update

---
+++ __manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Pos closing control",
+
+    'summary': """ Pos closing control """,
+
+    'description': """
+        Cierre ciego
+    """,
+
+    'author': "JS",
+    'website': "",
+
+    'category': 'Sales/Point of Sale',
+    'version': '1.01',
+
+    'depends': ['point_of_sale'],
+
+    'data': [
+
+    ],
+    'assets':{
+        'web.assets_qweb': [
+            'quemen/static/src/xml/**/*',
+        ],    
+
+    },
+    'license': 'LGPL-3',
+}
